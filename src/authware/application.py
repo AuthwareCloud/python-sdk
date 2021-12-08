d0c6e554ed2676018d850d96196e27b4740ba96f
dnlib <59972716+dnlib@users.noreply.github.com>
2021-12-08 10:30:37 +0000

    fix: storage, application data fetching

--- src/authware/application.py
+++ src/authware/application.py
@@ -1,34 +1,57 @@
+import re
 import aiohttp
-import json
+import requests
 
+from uuid import UUID
 from authware.hwid import HardwareId
 from authware.user import User
 from authware.utils import Authware
-from authware.storage import AuthStorage
+
+
+def __from_str(x):
+    assert isinstance(x, str)
+    return x
+
+
+def __to_class(c, x):
+    assert isinstance(x, c)
+    return x.to_dict()
+
 
 class Application:
-    version = None
-    
     hwid = HardwareId()
-    storage = AuthStorage()
+    is_init = False
     
-    def __init__(self, app_id: str, version: str):
-        Authware.app_id = app_id
+    def __init__(self, id: str, version: str):
+        self.id = id
+        self.version = version
+        
+        Authware.app_id = id
+        Authware.version = version
         Authware.headers = {
             "X-Authware-Hardware-ID": self.hwid.get_id(),
             "X-Authware-App-Version": version,
             "User-Agent": f"AuthwarePython/{Authware.wrapper_ver}",
-            "Authorization": f"Bearer {self.storage.read_auth_token()}"
+            "Authorization": f"Bearer {Authware.auth_token}"
         }
-        Authware.version = version
         
+        app = self.__fetch_app()
+
+        self.name = app["name"]
+        self.date_created = app["date_created"]
+
+    def __fetch_app(self) -> dict:
+        fetch_payload = {
+            "app_id": self.id
+        }
+        
+        # There has got to be a better way of doing this
+        req = requests.post(Authware.base_url + "/app", json=fetch_payload, headers=Authware.headers)
+        return Authware.check_response_sync(req)
+    
     async def authenticate(self, username: str, password: str) -> dict:
-        auth_storage = self.storage.read_auth_token()
-        if auth_storage is not None:
-            return { 'token': auth_storage }
-        
         auth_payload = {
-            "app_id": self.app_id,
+            "app_id": self.id,
             "username": username,
             "password": password
         }
@@ -40,14 +63,14 @@ class Application:
             async with session.post("/user/auth", json=auth_payload) as resp:
                 auth_response = await Authware.check_response(resp)
                 
-        self.auth_token = auth_response["auth_token"]   
-        self.storage.save_auth_token(self.auth_token)
-        
+        Authware.auth_token = auth_response["auth_token"]
+        Authware.regenerate_headers()
+            
         return auth_response
     
     async def create_user(self, username: str, email: str, password: str, token: str) -> dict:
         create_payload = {
-            "app_id": Authware.app_id,
+            "app_id": self.id,
             "username": username,
             "email_address": email,
             "password": password,
@@ -65,7 +88,7 @@ class Application:
     
     async def get_variables(self) -> dict:
         variable_payload = {
-            "app_id": Authware.app_id
+            "app_id": self.id
         }
         
         variable_response = None
@@ -89,5 +112,27 @@ class Application:
                 profile_response = await Authware.check_response(resp)
                 
         return User.from_dict(profile_response)
-        
-        
+
+    @staticmethod
+    def __from_dict(obj):
+        assert isinstance(obj, dict)
+        name = __from_str(obj.get("name"))
+        id = UUID(obj.get("id"))
+        version = __from_str(obj.get("version"))
+        date_created = __from_str(obj.get("date_created"))
+        return Application(name, id, version, date_created)
+
+    def __to_dict(self):
+        result = {}
+        result["name"] = __from_str(self.name)
+        result["id"] = str(self.id)
+        result["version"] = __from_str(self.version)
+        result["date_created"] = __from_str(self.date_created)
+        return result
+
+def __application_from_dict(s):
+    return Application.from_dict(s)
+
+
+def __application_to_dict(x):
+    return __to_class(Application, x)
--- src/authware/utils.py
+++ src/authware/utils.py
@@ -1,16 +1,19 @@
+import asyncio
+
 from aiohttp.client_reqrep import ClientResponse
+from requests.models import Response
 from authware.hwid import HardwareId
-from authware.storage import AuthStorage
 from authware.exceptions import UpdateRequiredException, ValidationException, AuthException
 
 class Authware:
-    wrapper_ver = "1.0.0.0"
+    wrapper_ver = "1.0.1"
     
     app_id = None    
     version = None
     
+    auth_token = None
+    
     hwid = HardwareId()
-    storage = AuthStorage()
     
     headers = {}
     base_url = "https://982c-2a02-c7f-76e2-7a00-58da-1abc-de5a-f3ae.ngrok.io"
@@ -20,15 +23,19 @@ class Authware:
         self.app_id = app_id
         self.headers = headers
         self.version = version
-        self.headers = {
-            "X-Authware-Hardware-ID": self.hwid.get_id(),
-            "X-Authware-App-Version": version,
-            "User-Agent": f"AuthwarePython/{self.wrapper.wrapper_ver}",
-            "Authorization": f"Bearer {self.storage.read_auth_token()}"
+        self.regenerate_headers()
+        
+    @staticmethod
+    def regenerate_headers():
+        Authware.headers = {
+            "X-Authware-Hardware-ID": Authware.hwid.get_id(),
+            "X-Authware-App-Version": Authware.version,
+            "User-Agent": f"AuthwarePython/{Authware.wrapper_ver}",
+            "Authorization": f"Bearer {Authware.auth_token}"
         }
         
     @staticmethod
-    async def check_response(resp: ClientResponse):
+    async def check_response(resp: ClientResponse) -> dict:
         response_json = await resp.json()
         
         if (resp.status == 426):
@@ -39,3 +46,25 @@ class Authware:
             raise AuthException(response_json['message'])
         
         return response_json
+    
+    @staticmethod
+    def check_response_sync(resp: Response) -> dict:
+        response_json = resp.json()
+        
+        if (resp.status_code == 426):
+            raise UpdateRequiredException(response_json["message"])
+        elif (resp.status_code == 400):
+            raise ValidationException(response_json["message"])
+        elif (resp.status_code != 200):
+            raise AuthException(response_json['message'])
+        
+        return response_json
+    
+    def once(func):
+        future = None
+        async def once_wrapper(*args, **kwargs):
+            nonlocal future
+            if not future:
+                future = asyncio.create_task(func(*args, **kwargs))
+            return await future
+        return once_wrapper
